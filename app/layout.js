011ff7b7ffec165eb454ebc3d365bb32001389c1
Arron Reed <circa187@hotmail.com>
2025-09-03 22:32:01 +0200

    Saves user to database

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,23 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./provider";
 
 export const metadata = {
   title: "AI Study Buddy",
   description: "Your AI study buddy helping you ace your exams",
 };
 
-const outfit= Outfit({subsets: ["latin"]});
+const outfit = Outfit({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={outfit.className}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={outfit.className}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Wassup G Dogg</h2>
       <Button variant="ghost">Subscribe</Button>
+
+      <UserButton />
     </div>
   );
 }
